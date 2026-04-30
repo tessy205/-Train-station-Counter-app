87c1af972277ec7e867796427f8224c1e62fad5d
Theresa Agbo-inedu <agboinedutheresa@gmail.com>
2026-04-30 02:11:20 +0100

    playing around
    might come back for the finishing touches

---
+++ game.js
@@ -0,0 +1,20 @@
+let mypoints = 3
+
+function add3points() {
+console.log(mypoints += 3)
+}
+
+function remove1point() {
+
+    console.log(mypoints -= 1 )
+
+}
+
+add3points()
+add3points()
+add3points()
+
+remove1point()
+remove1point()
+
+console.log(mypoints)
